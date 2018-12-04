f48f53547ce7a71d0e9a695575c98ada61b8f915
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-12-03 20:27:00 -0800

    Exports qrModel

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ module.exports.Person = require('./lib/core-types/Person').default;
 
 module.exports.q = require('./lib/query/Query').default;
 module.exports.qr = require('./lib/query/QueryRule').default;
+module.exports.qrModel = require('./lib/Model').qrModel;
 module.exports.t = require('tcomb-validation');
 module.exports.tModel = require('./lib/Model').tModel;
