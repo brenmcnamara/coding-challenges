bfacf1e118a353f161085218e5a40284a16eb4a4
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-12-03 20:02:41 -0800

    Exports Query and QueryRule from module

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ module.exports = {};
 module.exports.Model = require('./lib/Model').default;
 module.exports.Person = require('./lib/core-types/Person').default;
 
+module.exports.q = require('./lib/query/Query').default;
+module.exports.qr = require('./lib/query/QueryRule').default;
+module.exports.t = require('tcomb-validation');
 module.exports.tModel = require('./lib/Model').tModel;
